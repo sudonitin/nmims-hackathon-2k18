b4cd24e2b4fadf335bbccfdfc91ccef0872dc7bd
globefire <sahun5774@gmail.com>
2018-10-07 20:40:03 +0530

    final product

---
+++ js/navbar.js
@@ -0,0 +1,40 @@
+
+function border1() {
+	document.getElementById('home').style.borderBottom = "solid #f04";
+	document.getElementById('quotes').style.borderBottom = "none";
+	document.getElementById('stories').style.borderBottom = "none";
+	document.getElementById('speak2us').style.borderBottom = "none";
+	document.getElementById('donatenow').style.borderBottom = "none";
+}
+
+function border2() {
+	document.getElementById('home').style.borderBottom = "none";
+	document.getElementById('quotes').style.borderBottom = "solid #f04";
+	document.getElementById('stories').style.borderBottom = "none";
+	document.getElementById('speak2us').style.borderBottom = "none";
+	document.getElementById('donatenow').style.borderBottom = "none";
+}
+
+function border3() {
+	document.getElementById('home').style.borderBottom = "none";
+	document.getElementById('quotes').style.borderBottom = "none";
+	document.getElementById('stories').style.borderBottom = "solid #f04";
+	document.getElementById('speak2us').style.borderBottom = "none";
+	document.getElementById('donatenow').style.borderBottom = "none";
+}
+
+function border4() {
+	document.getElementById('home').style.borderBottom = "none";
+	document.getElementById('quotes').style.borderBottom = "none";
+	document.getElementById('stories').style.borderBottom = "none";
+	document.getElementById('speak2us').style.borderBottom = "solid #f04";
+	document.getElementById('donatenow').style.borderBottom = "none";
+}
+
+function border5() {
+	document.getElementById('home').style.borderBottom = "none";
+	document.getElementById('quotes').style.borderBottom = "none";
+	document.getElementById('stories').style.borderBottom = "none";
+	document.getElementById('speak2us').style.borderBottom = "none";
+	document.getElementById('donatenow').style.borderBottom = "solid #f04";
+}
